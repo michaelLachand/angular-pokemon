492f1980c55cfee7bd7b5ca535978939e2993690
wes35 <lachand_michael@hotmail.com>
2022-04-22 16:25:15 +0200

    ajout service

--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {Pokemon} from "../pokemon";
 import {POKEMONS} from "../mock-pokemon-list";
+import {PokemonService} from "../pokemon.service";
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
 })
 export class DetailPokemonComponent implements OnInit {
 
@@ -15,13 +14,14 @@ export class DetailPokemonComponent implements OnInit {
   pokemon: Pokemon|undefined;
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private ps: PokemonService
+  ) { }
 
   ngOnInit(): void {
-    this.pokemonList = POKEMONS;
     const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
     if(pokemonId){
-      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+      this.pokemon = this.ps.getPokemonById(+pokemonId);
     }
   }
 
--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Pokemon} from "../pokemon";
 import {POKEMONS} from "../mock-pokemon-list";
 import {Router} from "@angular/router";
+import {PokemonService} from "../pokemon.service";
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
 })
-export class ListPokemonComponent {
+export class ListPokemonComponent implements OnInit{
 
-  pokemonList: Pokemon[] = POKEMONS;
+  pokemonList: Pokemon[];
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private ps: PokemonService) {
+  }
+
+  ngOnInit(){
+    this.pokemonList = this.ps.getPokemonList();
   }
 
   goToPokemon(pokemon: Pokemon){
